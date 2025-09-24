211ac6307ab069b690d0e0a87603c13ae00b667d
mhhoss <mhossza@gmail.com>
2025-09-24 11:33:58 +0330

    Update CRUD operations

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
-from typing import Optional
 from datetime import datetime
 from fastapi import FastAPI, HTTPException
-
 
 from models import Note
 
--- app/models.py
+++ app/models.py
@@ -4,8 +4,9 @@ from pydantic import BaseModel, Field
 
 
 class Note(BaseModel):
-    title: str
+    name: str
     content: str
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: Optional[datetime] = None
+    topic: str
 

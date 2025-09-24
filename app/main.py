b1eba03d4ae9909e77690c05f415b5526199c43f
mhhoss <mhossza@gmail.com>
2025-09-24 11:36:12 +0330

    Update CRUD operations

--- app/main.py
+++ app/main.py
@@ -1,52 +1,75 @@
+from cgitb import text
 from datetime import datetime
+import uuid
 from fastapi import FastAPI, HTTPException
+from typing import List
 
 from models import Note
 
-app = FastAPI()
+app = FastAPI(
+    title= "Note Taking API", description= "This is a simple RESTful API service to taking note"
+)
 
 notes_db = []
 
 
 @app.get("/")
-def root():
-    return {"message: API is running..."}
+async def root():
+    return {"Message: API is running 🌪️"}
 
 
-@app.post("/notes/", response_model= Note)  # ذخیره نوت جدید
-def create_items(note: Note):
+@app.post("/notes/", response_model= Note)
+async def create_note(note: Note):
+    '''
+    ذخیره نوت جدید با آیدی یکتا
+    '''
+    note.id = str(uuid.uuid4())
     note.created_at = datetime.now()
     note.updated_at = note.created_at
     notes_db.append(note)
     return note
 
 
-@app.get(response_model= list[Note])
-def list_notes():
+@app.get("/notes", response_model= List[Note])
+async def list_notes():
+    '''
+    گرفتن کل نت ها
+    '''
     return notes_db
 
 
-@app.get("/items/{note_id}", response_model=Note)  # برگردوندن نوت براساس اندیس
-def get_note_by_id(note_id: int) -> str:
-    if 0 <= note_id <= len(notes_db):
-        return notes_db[note_id]
-    else:
-        raise HTTPException(stats_code=404, detail=f"note {note_id}, not found!")
+@app.get("/notes/{note_id}", response_model=Note)
+async def get_note_by_id(note_id: str):
+    '''
+    برگردوندن نوت براساس آیدی
+    '''
+    found_note = text((note for note in notes_db if note.id == note_id), None)
+    if found_note:
+        return found_note
+    raise HTTPException(status_code=404, detail=f"Note {note_id}, not found!")
 
 
-@app.put("/items/{note_id}", response_model=Note)
-def update_note(note_id: int, updated_note: None):
-    if 0 <= note_id <= len(notes_db):
-        notes_db[note_id].title = updated_note.title
-        notes_db[note_id].content = updated_note.content
-        notes_db[note_id].updated_at = datetime.now()
-        return notes_db[note_id]
-    raise HTTPException(stats_code=404, detail=f"note {note_id}, not found!")
+@app.put("/notes/{note_id}", response_model=Note)
+async def update_note(note_id: str, updated_note: None):
+    '''
+    بروز رسانی نت ها
+    '''
+    found_note = text((note for note in notes_db if note.id == note_id), None)
+    if found_note:
+        found_note.title = updated_note.title
+        found_note.content = updated_note.content
+        found_note.updated_at = datetime.now()
+        return found_note
+    raise HTTPException(status_code=404, detail=f"note {note_id}, not found!")
 
 
 @app.delete("/notes/{note_id}", response_model=Note)
-def delete_note(note_id: int):
-    if 0 <= note_id <= len(notes_db):
-        notes_db.pop(note_id)
-        return {f"{note_id}, deleted successfully"}
+async def delete_note(note_id: str):
+    '''
+    حذف نت با آیدی یکتا برای مرتب سازی
+    '''
+    if 0 <= note_id < len(notes_db):
+        deleted_note = notes_db.pop(note_id)
+        return deleted_note
     raise HTTPException(status_code=404, detail="Note not found!")
+
--- app/models.py
+++ app/models.py
@@ -1,12 +1,21 @@
 from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, Field
+import uuid
 
 
+# مدلسازی داده ها
 class Note(BaseModel):
+    id: str
     name: str
     content: str
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: Optional[datetime] = None
-    topic: str
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if not self.id:
+            self.id = str(uuid.uuid4())
+            '''
+            اگر آیدی مشخص نشده باشه اینجا ایجاد و مدلسازی میشه
+            '''

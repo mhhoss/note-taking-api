8ec89c0fb3c124483fd587499025997f760d1fd4
mhhoss <mhossza@gmail.com>
2025-09-28 13:26:58 +0330

    update main file

--- app/main.py
+++ app/main.py
@@ -1,75 +1,26 @@
-from cgitb import text
-from datetime import datetime
-import uuid
-from fastapi import FastAPI, HTTPException
-from typing import List
+from fastapi import FastAPI
+from app.routers.notes import router
+from app.db import connect_to_db
 
-from models import Note
+
+# برای مدیریت چرخش کار API
+def lifespan(app: FastAPI):
+    connect_to_db()  # راه اندازی دیتابیس
+    yield
+    pass
+
 
 app = FastAPI(
-    title= "Note Taking API", description= "This is a simple RESTful API service to taking note"
+    name= "Note Taking API",
+    description= "This is a simple RESTful API service to take notes",
+    lifespan=lifespan
 )
 
-notes_db = []
+
+# add endpoints
+app.include_router(router)
 
 
 @app.get("/")
-async def root():
+def root():
     return {"Message: API is running 🌪️"}
-
-
-@app.post("/notes/", response_model= Note)
-async def create_note(note: Note):
-    '''
-    ذخیره نوت جدید با آیدی یکتا
-    '''
-    note.id = str(uuid.uuid4())
-    note.created_at = datetime.now()
-    note.updated_at = note.created_at
-    notes_db.append(note)
-    return note
-
-
-@app.get("/notes", response_model= List[Note])
-async def list_notes():
-    '''
-    گرفتن کل نت ها
-    '''
-    return notes_db
-
-
-@app.get("/notes/{note_id}", response_model=Note)
-async def get_note_by_id(note_id: str):
-    '''
-    برگردوندن نوت براساس آیدی
-    '''
-    found_note = text((note for note in notes_db if note.id == note_id), None)
-    if found_note:
-        return found_note
-    raise HTTPException(status_code=404, detail=f"Note {note_id}, not found!")
-
-
-@app.put("/notes/{note_id}", response_model=Note)
-async def update_note(note_id: str, updated_note: None):
-    '''
-    بروز رسانی نت ها
-    '''
-    found_note = text((note for note in notes_db if note.id == note_id), None)
-    if found_note:
-        found_note.title = updated_note.title
-        found_note.content = updated_note.content
-        found_note.updated_at = datetime.now()
-        return found_note
-    raise HTTPException(status_code=404, detail=f"note {note_id}, not found!")
-
-
-@app.delete("/notes/{note_id}", response_model=Note)
-async def delete_note(note_id: str):
-    '''
-    حذف نت با آیدی یکتا برای مرتب سازی
-    '''
-    if 0 <= note_id < len(notes_db):
-        deleted_note = notes_db.pop(note_id)
-        return deleted_note
-    raise HTTPException(status_code=404, detail="Note not found!")
-

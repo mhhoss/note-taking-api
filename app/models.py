acf16fc98d02b26b73cdbd244345822a8ddbb592
mhhoss <mhossza@gmail.com>
2025-10-09 20:40:27 +0330

    Refactor models: Add is_edited field to Note model for timestamp

--- app/models.py
+++ app/models.py
@@ -8,8 +8,17 @@ class Note(BaseModel):
     id: str = Field(default_factory= lambda: str(uuid.uuid4()))
     name: str
     content: str | None = None
-    created_at: str = Field(default_factory= lambda: jdatetime.datetime.now().strftime('%Y/%m/%d %H:%M'))
+    created_at: str = Field(
+        default_factory= lambda: jdatetime.datetime.now().strftime('%Y/%m/%d %H:%M')
+        )
+    is_edited: int = 0
     '''
     نوشتن اسم یادداشت برای ثبت شدن اجباریه ولی متن میتونه خالی باشه
     بصورت خودکار آیدی و تاریخ ثبت برای یادداشت ها ساخته میشه
+    
+    ->  همچنین یک فیلد تاریخ وجود داره که همیشه آخرین زمان تغییر رو نشون میده
+        و بسته به اینکه یادداشت تازه ساخته شده یا ویرایش شده، در خروجی نمایش
+        created_at / edited_at
+        0 = تازه ساخته شده
+        1 = ویرایش شده
     '''
